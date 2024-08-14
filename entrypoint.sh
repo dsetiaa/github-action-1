18e237cb992c7301abbeaf31281d626c8e9886b6
D-setia <dsetia@cs.iitr.ac.in>
2024-08-14 19:08:22 +0530

    change env var names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh -l
 
-export INPUT_GPUS = $1
-export INPUT_GPU_TYPE = $2
-export INPUT_CPU = $3
-export INPUT_MEMORY = $4
-export INPUT_MIN_SCALE = $5
-export INPUT_MAX_SCALE = $6
-export INPUT_ENV = $7
+export IP_GPUS = $1
+export IP_GPU_TYPE = $2
+export IP_CPU = $3
+export IP_MEMORY = $4
+export IP_MIN_SCALE = $5
+export IP_MAX_SCALE = $6
+export IP_ENV = $7
 
 echo "Inputs: $1, $2, $3, $4, $5, $6, $7"
 
-echo "gpus: $INPUT_GPUS"
-echo "gpu_type: $INPUT_GPU_TYPE"
-echo "cpu: $INPUT_CPU"
-echo "memory: $INPUT_MEMORY"
-echo "min_scale: $INPUT_MIN_SCALE"
-echo "max_scale: $INPUT_MAX_SCALE"
-echo "env: $INPUT_ENV"
+echo "gpus: $IP_GPUS"
+echo "gpu_type: $IP_GPU_TYPE"
+echo "cpu: $IP_CPU"
+echo "memory: $IP_MEMORY"
+echo "min_scale: $IP_MIN_SCALE"
+echo "max_scale: $IP_MAX_SCALE"
+echo "env: $IP_ENV"
 
 
 aws eks update-kubeconfig --name divyanshu --region us-east-1       
-tensorkube deploy --gpus $INPUT_GPUS --gpu-type $INPUT_GPU_TYPE --cpu $INPUT_CPU --memory $INPUT_MEMORY --min-scale $INPUT_MIN_SCALE --max-scale $INPUT_MAX_SCALE --env $INPUT_ENV
+tensorkube deploy --gpus $IP_GPUS --gpu-type $IP_GPU_TYPE --cpu $IP_CPU --memory $IP_MEMORY --min-scale $IP_MIN_SCALE --max-scale $IP_MAX_SCALE --env $IP_ENV
