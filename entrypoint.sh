6c89871a9ef7fdc6aa7f3ef06567c0d25121f625
D-setia <dsetia@cs.iitr.ac.in>
2024-08-14 19:16:55 +0530

    remove vars from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,4 @@
 #!/bin/sh -l
 
-IP_GPUS = $1
-IP_GPU_TYPE = $2
-IP_CPU = $3
-IP_MEMORY = $4
-IP_MIN_SCALE = $5
-IP_MAX_SCALE = $6
-IP_ENV = $7
-
-echo "Inputs: $1, $2, $3, $4, $5, $6, $7"
-
-echo "gpus: $IP_GPUS"
-echo "gpu_type: $IP_GPU_TYPE"
-echo "cpu: $IP_CPU"
-echo "memory: $IP_MEMORY"
-echo "min_scale: $IP_MIN_SCALE"
-echo "max_scale: $IP_MAX_SCALE"
-echo "env: $IP_ENV"
-
-
 aws eks update-kubeconfig --name divyanshu --region us-east-1       
-tensorkube deploy --gpus $IP_GPUS --gpu-type $IP_GPU_TYPE --cpu $IP_CPU --memory $IP_MEMORY --min-scale $IP_MIN_SCALE --max-scale $IP_MAX_SCALE --env $IP_ENV
+tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
