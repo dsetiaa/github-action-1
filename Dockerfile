cf227e9ee308c3bcb13af41b792b0471fec99c8a
D-setia <dsetia@cs.iitr.ac.in>
2024-08-14 14:03:58 +0530

    Remove aws token requirement

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 # Container image that runs your code
 FROM python:3.10-slim
 
-RUN apt-get install --no-cache aws-cli kubectl  && pip3 install tensorkube
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends awscli && \
+    pip3 install tensorkube && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,6 @@ min_scale = $5
 max_scale = $6
 env = $7
 
-AWS_ACCESS_KEY_ID = $8
-AWS_SECRET_ACCESS_KEY = $9
-AWS_SESSION_TOKEN = $10
-
 echo "gpus: $gpus"
 echo "gpu_type: $gpu_type"
 echo "cpu: $cpu"
@@ -20,10 +16,6 @@ echo "min_scale: $min_scale"
 echo "max_scale: $max_scale"
 echo "env: $env"
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
-export AWS_DEFAULT_REGION=us-east-1
 
 aws eks update-kubeconfig --name divyanshu --region us-east-1       
 tensorkube deploy --gpus $gpus --gpu-type $gpu_type --cpu $cpu --memory $memory --min-scale $min_scale --max-scale $max_scale --env $env
