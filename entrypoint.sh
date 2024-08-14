0a4c74b6924555e7b9d13943f956086409d0608b
D-setia <dsetia@cs.iitr.ac.in>
2024-08-14 19:40:47 +0530

    add --env tag conditionally

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 
-aws eks update-kubeconfig --name divyanshu --region us-east-1       
-tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
+aws eks update-kubeconfig --name divyanshu --region us-east-1   
+if [ -n "$7" ]; then
+    tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
+else
+    tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6
+fi
