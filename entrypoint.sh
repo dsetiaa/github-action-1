84a5b4ba190a90f3f269246ce710f887dd405243
D-setia <dsetia@cs.iitr.ac.in>
2024-08-14 19:12:34 +0530

    change env var names 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
-export IP_GPUS = $1
-export IP_GPU_TYPE = $2
-export IP_CPU = $3
-export IP_MEMORY = $4
-export IP_MIN_SCALE = $5
-export IP_MAX_SCALE = $6
-export IP_ENV = $7
+IP_GPUS = $1
+IP_GPU_TYPE = $2
+IP_CPU = $3
+IP_MEMORY = $4
+IP_MIN_SCALE = $5
+IP_MAX_SCALE = $6
+IP_ENV = $7
 
 echo "Inputs: $1, $2, $3, $4, $5, $6, $7"
 
