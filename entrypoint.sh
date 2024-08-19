13f38615e9b67799b5b182d1b91228926b740671
D-setia <dsetia@cs.iitr.ac.in>
2024-08-19 12:22:29 +0530

    update cluster name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-aws eks update-kubeconfig --name divyanshu --region us-east-1   
+aws eks update-kubeconfig --name tensorkube --region us-east-1   
 if [ -n "$7" ]; then
     tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
 else
