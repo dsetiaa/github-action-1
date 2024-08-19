bef2cdf727f648ed5f362e08599e26335ce4509f
D-setia <dsetia@cs.iitr.ac.in>
2024-08-19 13:39:14 +0530

    handle null gpu_type

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,17 @@
 aws eks update-kubeconfig --name tensorkube --region us-east-1 
 ls -l
 cd $8 
-if [ -n "$7" ]; then
-    tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
+
+if [-n "$2"]; then
+    if [ -n "$7" ]; then
+        tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
+    else
+        tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6
+    fi
 else
-    tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6
+    if [ -n "$7" ]; then
+        tensorkube deploy --gpus $1 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
+    else
+        tensorkube deploy --gpus $1 --cpu $3 --memory $4 --min-scale $5 --max-scale $6
+    fi
 fi
