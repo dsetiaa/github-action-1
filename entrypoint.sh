6eb6a457ed82a30f73087b08d30b446c82f92c46
D-setia <dsetia@cs.iitr.ac.in>
2024-08-19 12:32:51 +0530

    add path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-aws eks update-kubeconfig --name tensorkube --region us-east-1   
+aws eks update-kubeconfig --name tensorkube --region us-east-1 
+ls -l
+cd $8 
 if [ -n "$7" ]; then
     tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
 else
