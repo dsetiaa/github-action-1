19c9c7585dddbc80a84da1644527cdc7612dbd21
D-setia <dsetia@cs.iitr.ac.in>
2024-08-27 19:28:17 +0530

    test secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,18 @@ cd $8
 export TENSORKUBE_TOKEN=$9
 export TENSORKUBE_SESSION_ID=${10}
 
+if [ "$TENSORKUBE_TOKEN" = "acad6d95-040b-4f16-871d-a76efe7e1502" ]; then
+    echo "TENSORKUBE_TOKEN matches"
+else
+    echo "TENSORKUBE_TOKEN does not match"
+fi
+
+if [ "$TENSORKUBE_SESSION_ID" = "fb1a2d65-0b71-4ade-b591-d5df92b44540" ]; then
+    echo "TENSORKUBE_SESSION_ID matches"
+else
+    echo "TENSORKUBE_SESSION_ID does not match"
+fi
+
 echo "TENSORKUBE_TOKEN:" $TENSORKUBE_TOKEN
 echo "TENSORKUBE_SESSION_ID:" $TENSORKUBE_SESSION_ID
 
